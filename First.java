7e013fed1bc964197dfb7a3bc2be8dd34ea969ed
Wagh-Pradnyesh <pradnyeshwagh@gmail.com>
2025-12-26 11:31:37 +0530

    First Java program

---
+++ First.java
@@ -0,0 +1,11 @@
+class FirstProject 
+{
+    public static void main(String[] args) {
+	System.out.println(" Welcome to My Basic Java Project!");
+        System.out.println("====================================");
+        System.out.println(" Author: Pradnyesh Wagh");
+        System.out.println("  This project is created for learning basic Java concepts.");
+        System.out.println("------------------------------------");
+        System.out.println(" Enjoy exploring the code! ");
+    }
+}
